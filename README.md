6b9a17596048e3c310b89b45a90476f4b5e08da9
EliabeJr <eliabejunior@hotmail.com.br>
2017-03-20 17:06:05 -0300

    Added to GH Pages

---
+++ README.md
@@ -0,0 +1,15 @@
+# alpha-framework
+
+Alpha Framework é um conjunto de soluções CSS que auxiliam desenvolvedores novatos a se habituar com frameworks. O framework ainda está em fase de desenvolvimento, ajudas serão bem-vindas!
+
+Requisitos:
+ - NodeJS
+ - Gulp
+
+
+# Como instalar:
+
+1. Clone o repositório
+2. Acesse a pasta via terminal
+3. Digite: npm i
+4. Enjoy!
---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp          = require("gulp");
+const sass          = require("gulp-sass");
+const notify        = require("gulp-notify");
+const cssmin        = require("gulp-clean-css");
+
+gulp.task('sass', function () {
+	return gulp.src("source/scss/**/*")
+			.pipe(sass())
+			.on("error", notify.onError({title:"Erro ao compilar CSS", message:"<%= error.message %>"}))
+			.pipe(gulp.dest("source/css"))
+});
+
+gulp.task('min-css', function(){
+  return gulp.src('source/css/style.css')
+    		.pipe(cssmin({compatibility: 'ie8'}))
+   			.pipe(gulp.dest('build/css'));
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch('source/scss/**/*', ['sass']);
+    gulp.watch('source/css/style.css', ['min-css']);
+});
+
+gulp.task('default',['sass','min-css','watch']);
