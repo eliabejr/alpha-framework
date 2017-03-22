1dd9e346a9db67911118994501d020fab80e1e86
EliabeJr <eliabejunior@hotmail.com.br>
2017-03-22 01:06:02 -0300

    Added JadeJS

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,48 @@ const gulp          = require("gulp");
 const sass          = require("gulp-sass");
 const notify        = require("gulp-notify");
 const cssmin        = require("gulp-clean-css");
+const imageop       = require('gulp-image-optimization');
+const jade          = require('gulp-jade');
 
-gulp.task('sass', function () {
+
+
+gulp.task('compile-html', function(){
+  gulp.src('source/templates/**/*.jade')
+    .pipe(jade())
+    .on("error", notify.onError({title:"Erro at compile HTML", message:"<%= error.message %>"}))
+    .pipe(gulp.dest('build'));
+  });
+
+gulp.task('compile-sass', function () {
 	return gulp.src("source/scss/**/*")
 			.pipe(sass())
-			.on("error", notify.onError({title:"Erro ao compilar CSS", message:"<%= error.message %>"}))
+			.on("error", notify.onError({title:"Error at compile CSS", message:"<%= error.message %>"}))
 			.pipe(gulp.dest("source/css"))
 });
 
-gulp.task('min-css', function(){
+gulp.task('minify-css', function(){
   return gulp.src('source/css/style.css')
     		.pipe(cssmin({compatibility: 'ie8'}))
+            .on("error", notify.onError({title:"Error at minify CSS", message:"<%= error.message %>"}))
    			.pipe(gulp.dest('build/css'));
+});
+
+gulp.task('optimize-images', function(cb) {
+    gulp.src(['source/img/**/*.png','source/img/**/*.jpg','source/img/**/*.gif','source/img/**/*.jpeg']).pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    }))
+    .on("error", notify.onError({title:"Error at optimize IMAGES", message:"<%= error.message %>"}))
+    .pipe(gulp.dest('build/img')).on('end', cb).on('error', cb);
 });
 
 
 gulp.task('watch', function() {
-    gulp.watch('source/scss/**/*', ['sass']);
-    gulp.watch('source/css/style.css', ['min-css']);
+    gulp.watch('source/templates/**/*', ['compile-html']);
+    gulp.watch('source/scss/**/*',      ['compile-sass']);
+    gulp.watch('source/css/style.css',  ['minify-css']);
+    gulp.watch('source/img/**/*',       ['optimize-images']);
 });
 
-gulp.task('default',['sass','min-css','watch']);
+gulp.task('default',['compile-html','compile-sass','minify-css','optimize-images','watch']);
