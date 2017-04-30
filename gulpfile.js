6ed378bb50e0c86bb8aa1cc1107401e1b262e41b
EliabeJr <eliabejunior@hotmail.com.br>
2017-04-30 01:35:59 -0300

    Insert Docs Page

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,25 @@ const pug           = require("gulp-pug");
 const sass          = require("gulp-sass");
 const notify        = require("gulp-notify");
 const cssmin        = require("gulp-clean-css");
+const browserSync   = require('browser-sync').create();
 const imageop       = require("gulp-image-optimization");
 
+//////////////////////
 
+var files = [
+  'source/html/**/*.pug',
+  'source/scss/**/*.scss',
+  'source/js/*.js'
+];
+
+var options = {
+  server: "build/"
+};
+
+///////////////////////
+
+
+// Compile PugJs Files
 gulp.task("compile-html", function(){
   gulp.src("source/html/*.pug")
     .pipe(pug())
@@ -13,19 +29,26 @@ gulp.task("compile-html", function(){
     .pipe(gulp.dest("build"));
   });
 
+
+// Compile SASS Files
 gulp.task("compile-sass", function () {
 	return gulp.src("source/scss/style.scss")
 			.pipe(sass())
 			.on("error", notify.onError({title:"Error at compile CSS", message:"<%= error.message %>"}))
 			.pipe(gulp.dest("source/css"))
+            .pipe(browserSync.stream());
 });
 
+
+// Copy new fonts to the BUILD
 gulp.task("copy-fonts", function() {
     gulp.src(["source/scss/modules/font-awesome/fonts/*","source/fonts/*"])
         .on("error", notify.onError({title:"Error at Copy Fonts", message:"<%= error.message %>"}))
         .pipe(gulp.dest("build/css/fonts"));
 });
 
+
+// Minify CSS Files
 gulp.task("minify-css", function(){
   return gulp.src("source/css/style.css")
     		.pipe(cssmin({compatibility: "ie8"}))
@@ -33,6 +56,8 @@ gulp.task("minify-css", function(){
    			.pipe(gulp.dest("build/css"));
 });
 
+
+// Optimize Images
 gulp.task("optimize-images", function(cb) {
     gulp.src(["source/img/**/*.png","source/img/**/*.jpg","source/img/**/*.gif","source/img/**/*.jpeg"]).pipe(imageop({
         optimizationLevel: 5,
@@ -43,6 +68,7 @@ gulp.task("optimize-images", function(cb) {
     .pipe(gulp.dest("build/img")).on("end", cb).on("error", cb);
 });
 
+// Minify Js
 gulp.task("compile-js", function() {
     gulp.src("source/js/*")
         .on("error", notify.onError({title:"Error at Compile Javascript", message:"<%= error.message %>"}))
@@ -58,4 +84,13 @@ gulp.task("watch", function() {
     gulp.watch("source/js/*",           ["compile-js"]);
 });
 
-gulp.task("default",["compile-html","compile-sass","copy-fonts","minify-css","optimize-images","compile-js","watch"]);
+// Browser reloading
+gulp.task('server', ['compile-sass'], function() {
+
+    browserSync.init(files, options)
+    
+    gulp.watch("src/sass/*.scss", ['sass']);
+    gulp.watch(files).on('change', browserSync.reload);
+});
+
+gulp.task("default",["compile-html","compile-sass","copy-fonts","minify-css","optimize-images","compile-js","watch","server"]);
